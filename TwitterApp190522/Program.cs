c0d9c0df3a788a5e62bc9943b323bf4a7bcd95e1
ioannis komninos <ikomninos@nyc.gr>
2022-05-19 19:06:47 +0300

    added tweetinvi library and credentials

--- TwitterApp190522/Program.cs
+++ TwitterApp190522/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace TwitterApp190522
 {
@@ -7,6 +9,9 @@ namespace TwitterApp190522
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var userCredentials = new TwitterCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            var userClient = new TwitterClient(userCredentials);
         }
     }
 }
