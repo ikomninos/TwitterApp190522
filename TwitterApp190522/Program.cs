2f9422ded89805d728d2e2f078ba8b77a77c9b18
ioannis komninos <ikomninos@nyc.gr>
2022-05-19 19:14:38 +0300

    added filter stream java

--- TwitterApp190522/Program.cs
+++ TwitterApp190522/Program.cs
@@ -6,12 +6,27 @@ namespace TwitterApp190522
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var userCredentials = new TwitterCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
             var userClient = new TwitterClient(userCredentials);
+
+
+            // Create a simple stream containing only tweets with the keyword France
+
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack("java");
+
+            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            {
+                Console.WriteLine(eventReceived.Tweet);
+            };
+
+            await stream.StartMatchingAnyConditionAsync();
+
+
         }
     }
 }
